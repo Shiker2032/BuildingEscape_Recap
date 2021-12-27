b2a8a44283d165bcdb101484777852356e6e40d3
Vladislav <53581923+Shiker2032@users.noreply.github.com>
2021-12-27 18:48:48 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# BuildingEscape_Recap
+
+Developed with Unreal Engine 4
---
+++ Source/BuildingEscape_Recap/BuildingEscape_RecapGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "BuildingEscape_RecapGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BUILDINGESCAPE_RECAP_API ABuildingEscape_RecapGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
